2f70c378db54ab6bd908f608f0574d25cd171a17
Ariel Diaz <arieldiaz.sistemas@gmail.composer>
2025-05-13 17:46:16 -0300

    update the procees and incorporate lib

---
+++ basic.py
@@ -0,0 +1,78 @@
+""""" Autor: Ariel Diaz
+     https://infrasoft.com.ar/
+     Salta Argentina    
+     Licence: Creative Commons 
+"""""
+""" This library uses Selenium"""
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+#driver = webdriver.Chrome()
+
+def selenium_input(identify="", value="",by="XPATH"):
+     try:
+        if(by=="XPATH"): 
+            input_element = WebDriverWait(driver, 3).until(
+                EC.presence_of_element_located((By.XPATH, identify))
+            )       
+            input_element = driver.find_element(By.XPATH, identify)            
+        
+        if(by=="ID"):
+            input_element = WebDriverWait(driver, 3).until(
+                EC.presence_of_element_located((By.ID, identify))
+            ) 
+            input_element = driver.find_element(By.ID, identify)            
+        
+        if(by=="NAME"):
+            input_element = WebDriverWait(driver, 3).until(
+                EC.presence_of_element_located((By.NAME, identify))
+            )
+            input_element = driver.find_element(By.NAME, identify)
+
+        if(by=="CLASS"):
+            input_element = WebDriverWait(driver, 3).until(
+                EC.presence_of_element_located((By.CLASS_NAME, identify))
+            )
+            input_element = driver.find_element(By.CLASS_NAME, identify)
+
+        input_element.send_keys(value)
+        return True
+
+     except:
+        print("Error in "+ identify)
+        #input()
+        return False
+
+def selenium_Button(identify="",by="XPATH"):
+    try:
+        if(by=="XPATH"):
+            button_element = WebDriverWait(driver, 3).until(
+                EC.presence_of_element_located((By.XPATH, identify))
+            ) 
+            button_element = driver.find_element(By.XPATH, identify)
+        
+        if(by=="ID"):
+            button_element = WebDriverWait(driver, 3).until(
+                EC.presence_of_element_located((By.ID, identify))
+            )
+            button_element = driver.find_element(By.ID, identify)
+
+        if(by=="NAME"):
+            button_element = WebDriverWait(driver, 3).until(
+                EC.presence_of_element_located((By.NAME, identify))
+            )
+            button_element = driver.find_element(By.NAME, identify)
+        if(by=="CLASS"):
+            button_element = WebDriverWait(driver, 3).until(
+                EC.presence_of_element_located((By.CLASS_NAME, identify))
+            )
+            button_element = driver.find_element(By.CLASS_NAME, identify)
+        button_element.click()
+        return True
+    except:
+        print("Error in "+ identify)
+        #input()
+        return False
--- walmart_scraper.py
+++ walmart_scraper.py
@@ -6,6 +6,8 @@ Mail: arieldiaz.sistemas (at) gmail.com
 """
 
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+import undetected_chromedriver as uc
 
 import time
 import csv
@@ -13,10 +15,33 @@ import csv
 from dotenv import load_dotenv
 import os 
 
+from basic import *
+
+def segurity():
+  print("configuracion de seguridad")
+
 def main():
-    input_file = os.getenv("INPUT_FILE")
-    print(input_file)
-    driver = webdriver.Chrome()
+    input_file = "model_file.csv" #os.getenv("INPUT_FILE")
+    web  = "https://walmart.com.mx/" #os.getenv("URL")
+    output_file = "outfile" #os.getenv("OUTPUT_FILE")
+    
+    driver = uc.Chrome()
+    os.system("cls")
+    driver.get(web)
+    
+    with open(input_file, "r") as f:
+      lector = csv.reader(f)
+      print("Start getTing data enter")
+      time.sleep(4)
+      input("Tomar datos")
+      segurity()
+      
+      for element in lector:
+        os.system("cls")
+        element = element.split(";")
+        print(element)
+        selenium_input('//*[@id="__next"]/div[1]/span/header/form/div/input',element[1])
+        selenium_Button('//*[@id="__next"]/div[1]/span/header/form/div/button[2]')    
 
 if __name__ == "__main__":
     main()
